ba5b30e5ef549753c94eafd01b595e2c2d612205
Adithyasai3 <adithya.asg@gmail.com>
2026-01-29 22:32:01 +0530

    Serve HTML page from Express and cleanup

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const PORT = 3000;
 
@@ -76,6 +78,10 @@ app.post("/login", (req, res) => {
     token: fakeToken,
   });
 });
+app.get("/page", (req, res) => {
+  res.sendFile(path.join(__dirname, "page.html"));
+});
+
 
 // server
 app.listen(PORT, () => {
